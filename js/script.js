07b61449f8c2e66596d10cff15585d8329b97ea5
Corpusruiz <Corpus.ruiz.93@gmail.com>
2023-11-10 14:18:13 +0100

    version final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
  // Aquí tu código
+const agregar = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+agregar.addEventListener('click', function(){
+    let cartel = prompt('agregar elemnto');
+    let cartelUno = document.createElement('li');
+    cartelUno.innerText = cartel;
+    lista.appendChild(cartelUno);
+});
